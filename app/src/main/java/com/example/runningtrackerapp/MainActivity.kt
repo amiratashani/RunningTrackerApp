129f45ac998ce03ddd5d1c3ee52513076a298b2d
Amir <amiratashanii@gmail.com>
2020-09-13 02:22:31 +0430

    add model run add DAO for database

--- app/src/main/java/com/example/runningtrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/runningtrackerapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.runningtrackerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.runningtrackerapp.db.Run
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val r = Run(timestamp = 10L)
+
     }
 }
