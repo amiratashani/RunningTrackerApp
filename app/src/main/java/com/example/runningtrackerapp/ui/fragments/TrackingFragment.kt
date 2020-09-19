5dc4e650b06b5b4b7df8c719e018b88bbdfcc008
amiratashani <amiratashanii@gmail.com>
2020-09-19 18:39:22 +0430

    add MapView and handel map lifecycle

--- app/src/main/java/com/example/runningtrackerapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runningtrackerapp/ui/fragments/TrackingFragment.kt
@@ -8,13 +8,16 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.runningtrackerapp.R
 import com.example.runningtrackerapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.EntryPoint
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment() {
 
     private val viewModel: MainViewModel by viewModels()
+    private var map: GoogleMap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +25,44 @@ class TrackingFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tracking, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
     }
 
 
